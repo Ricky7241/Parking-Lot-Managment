bd0d87783dedbb69e4e125f56fb7853025208495
samanpreet1 <145691412+samanpreet1@users.noreply.github.com>
2024-03-07 12:16:12 -0500

    Add files via upload
    this file have two fixed admin

--- Project/Project/PROJECT/admin_login_process.php
+++ Project/Project/PROJECT/admin_login_process.php
@@ -4,30 +4,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (isset($_POST["username"]) && isset($_POST["password"]) && !empty($_POST["username"]) && !empty($_POST["password"])) {
       
-        $admin_username = "admin";
-        $admin_password = "admin123";
+        // Define admin users
+        $admin_users = array(
+            "admin" => "admin123",
+            "admin2" => "admin123"
+        );
 
-         //
+        // Get entered credentials
         $entered_username = $_POST["username"];
         $entered_password = $_POST["password"];
 
-         
-        if ($entered_username === $admin_username && $entered_password === $admin_password) {
-          
+        // Check if entered credentials match any admin user
+        if (array_key_exists($entered_username, $admin_users) && $admin_users[$entered_username] === $entered_password) {
             header("Location: admin_dashboard.php");
             exit();
         } else {
-           
             header("Location: admin_login.php?error=invalid_credentials");
             exit();
         }
     } else {
-  
         header("Location: admin_login.php?error=missing_credentials");
         exit();
     }
 } else {
-
     header("Location: admin_login.php");
     exit();
 }
